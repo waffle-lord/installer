cb5ee9649d53ea46373ff6d204bf97298081e4cd
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-11-17 19:02:04 -0800

    Fix Steam edition validation even more
    Turns out the registry isn't a good source of truth for Steam game installs

--- SPTInstaller/Helpers/PreCheckHelper.cs
+++ SPTInstaller/Helpers/PreCheckHelper.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using Microsoft.Win32;
 using SPTInstaller.Models;
 
@@ -9,8 +11,8 @@ public static class PreCheckHelper
 {
     private const string registryInstall =
         @"Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\EscapeFromTarkov";
-    private const string steamRegistryInstall =
-        @"Software\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 3932890";
+    private const string steamPathRegistryKey = @"Software\Wow6432Node\Valve\Steam";
+    private const string tarkovAppId = "3932890";
     
     public static string? DetectOriginalGamePath()
     {
@@ -20,14 +22,11 @@ public static class PreCheckHelper
 
         // We prioritize the Steam version, as the Steam CDN is faster for updates, and if someone
         // owns it on both platforms, there's a better chance their Steam version is up to date
-        var steamUninstallValue = Registry.LocalMachine.OpenSubKey(steamRegistryInstall, false)
-            ?.GetValue("InstallLocation");
-        if (steamUninstallValue != null && 
-            steamUninstallValue is string steamUninstallStringValue &&
-            Path.Exists(Path.Combine(steamUninstallStringValue, "build")))
+        var steamGamePath = DetectSteamGamePath();
+        if (steamGamePath != null && 
+            Path.Exists(Path.Combine(steamGamePath, "build")))
         {
-            var steamDirInfo = new DirectoryInfo(steamUninstallStringValue);
-            var steamBuildDir = Path.Combine(steamDirInfo.FullName, "build");
+            var steamBuildDir = Path.Combine(steamGamePath, "build");
             return Path.TrimEndingDirectorySeparator(steamBuildDir);
         }
 
@@ -41,7 +40,92 @@ public static class PreCheckHelper
         
         return Path.TrimEndingDirectorySeparator(info.FullName);
     }
-    
+
+    /**
+     * Steam doesn't update it's libraryfolders.vdf on new game install, so we need
+     * to chain the following steps to find where Tarkov is installed:
+     *   - Load all steam library paths from libraryfolders.vdf
+     *   - Loops through all libraries, and look for Tarkov's appmanifest
+     *   - Extract the `installdir` from Tarkov's appmanifest
+     *   - Build the final path as `library + steamapps + common + installdir`
+     */
+    private static string? DetectSteamGamePath()
+    {
+        // We can't detect the installed path on non-Windows
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            return null;
+
+        var steamPath = GetSteamInstallPath();
+        if (string.IsNullOrEmpty(steamPath))
+        {
+            return null;
+        }
+
+        var libraryFoldersPath = Path.Combine(steamPath, "steamapps", "libraryfolders.vdf");
+        var steamLibraryPaths = ExtractVdfFieldsByName(libraryFoldersPath, "path");
+        if (steamLibraryPaths.Count == 0)
+        {
+            return null;
+        }
+
+        return FindTarkovInSteamLibraries(steamLibraryPaths);
+    }
+
+    private static string? GetSteamInstallPath()
+    {
+        // We can't detect the installed path on non-Windows
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            return null;
+
+        var steamPath = string.Empty;
+        using (var steamReg = Registry.LocalMachine.OpenSubKey(steamPathRegistryKey, false))
+        {
+            if (steamReg != null)
+            {
+                steamPath = steamReg.GetValue("InstallPath")?.ToString();
+            }
+        }
+
+        return steamPath;
+    }
+
+    private static string? FindTarkovInSteamLibraries(List<string> steamLibraryPaths)
+    {
+        var appManifestFilename = $"appmanifest_{tarkovAppId}.acf";
+        foreach (var steamLibraryPath in steamLibraryPaths)
+        {
+            var appManifestPath = Path.Combine(steamLibraryPath, "steamapps", appManifestFilename);
+            if (!File.Exists(appManifestPath))
+            {
+                continue;
+            }
+
+            var installDirFields = ExtractVdfFieldsByName(appManifestPath, "installdir");
+            if (installDirFields.Count > 0)
+            {
+                return Path.Combine(steamLibraryPath, "steamapps", "common", installDirFields[0]);
+            }
+        }
+
+        return null;
+    }
+
+    private static List<string> ExtractVdfFieldsByName(string vdfPath, string fieldName)
+    {
+        var fieldValues = new List<string>();
+        var libraryPathPattern = $@"""{fieldName}""\s+""(.*)""";
+        foreach (var line in File.ReadLines(vdfPath))
+        {
+            var match = Regex.Match(line, libraryPathPattern);
+            if (match.Success)
+            {
+                fieldValues.Add(Regex.Unescape(match.Groups[1].Value));
+            }
+        }
+
+        return fieldValues;
+    }
+
     public static Result DetectOriginalGameVersion(string gamePath)
     {
         try
